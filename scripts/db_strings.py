9cd97576fbb5a00080006d6cc6c5af792374b841
Josh Mize <jgmize@gmail.com>
2015-01-07 00:21:52 -0600

    Fix and move db_strings

---
+++ scripts/db_strings.py
@@ -0,0 +1,19 @@
+import os
+
+from django.conf import settings
+
+from masterfirefoxos.base.utils import pages_l10n_template
+
+
+def run(*args):
+    if args:
+        filename = args[0]
+    else:
+        filename = os.path.join(settings.BASE_DIR, 'db-strings.txt')
+    print(filename)
+    template_text = pages_l10n_template()
+    if filename == '-':
+        self.stdout.write(template_text)
+    else:
+        with open(filename, 'w') as f:
+            f.write(template_text)
