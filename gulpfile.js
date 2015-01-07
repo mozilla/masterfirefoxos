e7627ee58e56a5c0be61c3e95b0a79e88362a347
Craig Cook <craigcook@gmail.com>
2015-01-07 11:34:48 -0800

    Rough home template

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,26 @@ var nib = require('nib');
 gulp.task('compress-base', function () {
   var filter = gulpFilter(['*', '!oldIE.styl']);
 
-  gulp.src('./masterfirefoxos/base/static/stylus/**.styl')
+  gulp.src('./masterfirefoxos/base/static/stylus/base.styl')
     .pipe(filter)
     .pipe(stylus({
       use: nib(),
       compress: true
     }))
     .pipe(concat('base.css'))
+    .pipe(gulp.dest('./masterfirefoxos/base/static/css'));
+});
+
+gulp.task('compress-home', function () {
+  var filter = gulpFilter(['*', '!oldIE.styl']);
+
+  gulp.src('./masterfirefoxos/base/static/stylus/home.styl')
+    .pipe(filter)
+    .pipe(stylus({
+      use: nib(),
+      compress: true
+    }))
+    .pipe(concat('home.css'))
     .pipe(gulp.dest('./masterfirefoxos/base/static/css'));
 });
 
@@ -26,7 +39,7 @@ gulp.task('compress-ie', function () {
     .pipe(gulp.dest('./masterfirefoxos/base/static/css'));
 });
 
-gulp.task('compress', ['compress-base', 'compress-ie']);
+gulp.task('compress', ['compress-base', 'compress-home', 'compress-ie']);
 
 gulp.task('watch', ['compress'], function () {
   gulp.watch("./masterfirefoxos/base/static/stylus/**.styl", ['compress']);
