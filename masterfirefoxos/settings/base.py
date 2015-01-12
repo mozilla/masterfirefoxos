25b0e242bc3d2c3f985548a6b5fb0f203dfd8ea5
Giorgos Logiotatidis <seadog@sealabs.net>
2015-01-12 15:30:22 +0200

    Populate versions dropdown from settings.

--- masterfirefoxos/settings/base.py
+++ masterfirefoxos/settings/base.py
@@ -159,6 +159,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
+    'masterfirefoxos.base.context_processors.settings',
 )
 
 LOCALE_PATHS = (
