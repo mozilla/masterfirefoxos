6bc57fc550adbaf22bf26349cfe75c24f39eba41
Giorgos Logiotatidis <seadog@sealabs.net>
2015-01-16 15:00:29 +0200

    Enable TinyMCE in textfields in admin content forms.

--- masterfirefoxos/base/admin.py
+++ masterfirefoxos/base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin, messages
 
 from feincms.module.page.models import Page
+from feincms.module.page.forms import PageAdminForm as PageAdminFormOld
 from feincms.module.page.admin import PageAdmin as PageAdminOld
 
 from .utils import copy_tree
@@ -8,12 +9,19 @@ from .utils import copy_tree
 
 admin.site.unregister(Page)
 
+class PageAdminForm(PageAdminFormOld):
+    class Media:
+        js = [
+            '//tinymce.cachefly.net/4.1/tinymce.min.js',
+            'js/init_tinymce.js'
+        ]
+
 
 class PageAdmin(PageAdminOld):
     save_on_top = True
     actions = ['copy_tree_admin_action']
     list_display = ['short_title', 'slug', 'is_visible_admin', 'template']
-
+    form = PageAdminForm
 
     def copy_tree_admin_action(self, request, queryset):
         if len(queryset) != 1:
