7ddb5612767027c7092279a2b9eb9d054bf75024
Giorgos Logiotatidis <seadog@sealabs.net>
2015-01-23 14:04:35 +0200

    Display version name instead of version slug in error message.

--- masterfirefoxos/base/views.py
+++ masterfirefoxos/base/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponseRedirect
 
 
 def home_redirect(request):
-    version_slug = settings.LOCALE_LATEST_VERSION_SLUG.get(
-        request.LANGUAGE_CODE)
-    if version_slug:
-        return HttpResponseRedirect(version_slug + '/')
+    version = settings.LOCALE_LATEST_VERSION.get(request.LANGUAGE_CODE)
+    if version:
+        return HttpResponseRedirect(version['slug'] + '/')
     return HttpResponseRedirect(
-        '/{}/{}/'.format('en', settings.LOCALE_LATEST_VERSION_SLUG['en']))
+        '/{}/{}/'.format('en', settings.LOCALE_LATEST_VERSION['en']['slug']))
