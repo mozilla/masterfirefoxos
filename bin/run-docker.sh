a64f877b82f97ae0969e443fcc3a7a7a3226c5cc
Giorgos Logiotatidis <seadog@sealabs.net>
2015-01-29 14:26:46 +0200

    Set revision.txt on both `git push` and `deis pull`.
    When using `git push` to push code to deis there is no git repository
    and therefore we cannot use git command to get the latest SHA. Instead
    we can use GIT_SHA env variable, which is automatically set by Deis.
    
    When using `deis pull` we can still rely on git.
    
    We moved the code that sets revision.txt to run-docker.sh to set
    revision.txt based on the environment.

--- bin/run-docker.sh
+++ bin/run-docker.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+
+if [ -e ./.git/HEAD ]
+then
+    cp ./.git/HEAD /app/masterfirefoxos/base/static/revision.txt
+else
+    echo $GIT_SHA > /app/masterfirefoxos/base/static/revision.txt
+fi
+
 ./bin/run-common.sh
 ./manage.py collectstatic --noinput
 gunicorn masterfirefoxos.wsgi:application -b 0.0.0.0:80 -w 2 --log-file -
