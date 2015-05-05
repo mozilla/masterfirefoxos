5954f3abe8646d76a538fb182b15d51c7e760b12
Josh Mize <jgmize@gmail.com>
2015-05-05 09:37:38 -0500

    Use deployed revision for acceptance tests

--- bin/jenkins/runacceptancetests.sh
+++ bin/jenkins/runacceptancetests.sh
@@ -9,6 +9,8 @@
 #
 set -ex
 
+REVISION=$(curl $BASE_URL/static/revision.txt)
+echo REVISION=$REVISION
 ENV_FILE=`mktemp`
 echo "DATABASE_URL=sqlite://" >> $ENV_FILE
 echo "DEBUG=False" >> $ENV_FILE
@@ -16,7 +18,7 @@ echo "ALLOWED_HOSTS=localhost" >> $ENV_FILE
 echo "SECRET_KEY=test" >> $ENV_FILE
 
 # Run Tests
-docker run --env-file=$ENV_FILE $DOCKER_REPOSITORY:$GIT_COMMIT py.test --acceptance --baseurl=$BASE_URL -n ${NUM_PROCS:-7}
+docker run --env-file=$ENV_FILE $DOCKER_REPOSITORY:$REVISION py.test --acceptance --baseurl=$BASE_URL -n ${NUM_PROCS:-7}
 
 # Delete temp file.
 rm -rf $ENV_FILE
