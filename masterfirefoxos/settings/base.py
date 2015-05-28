81a8540dbccddc182776f0fba7047acb57114255
Benjamin Sternthal <ben@devpatch.com>
2015-05-28 16:41:26 -0700

    Fix Bug 1169007: Add ES 2.0 Videos

--- masterfirefoxos/settings/base.py
+++ masterfirefoxos/settings/base.py
@@ -417,31 +417,37 @@ LOCALIZED_YOUTUBE_ID = {
     # 2.0
     '9WWo33tWwNw': {
         'de': 'gfHbcpi3Rc4',
+        'es': 'dBOY2I8jbNw',
         'fr': 'DPCDIkF9dN0',
         'pt-br': 'sJaHzQxiBY4',
     },
     'jrcmZMkgHVU': {
         'de': '_8dCK2zPAbA',
+        'es': 'izI5jEUoyzE',
         'fr': 'RMnt-nfmytY',
         'pt-br': 'gAUfzgevRrw',
     },
     'i0UY48l-nXU': {
         'de': '5VlR0J3pMKg',
+        'es': 'JeLDCekefps',
         'fr': 'gAsGJkBWtKo',
         'pt-br': 'nR0IcH4PKWs',
     },
     'blOcJrnirn0': {
         'de': 'Z6aBrtRgTqQ',
+        'es': '-5rUB-HPT-g',
         'fr': 'oVVV6qYlNEA',
         'pt-br': 'beML24YmXVo',
     },
     '6ybgSsGZZU4': {
         'de': 'irGl0DmNgmo',
+        'es': 'X-HPLiHHzUw',
         'fr': 'IZ9CnX38ai8',
         'pt-br': 'kfO2Vi9Jq_o',
     },
     'g5heblzMevA': {
         'de': 'awg7mT-edwM',
+        'es': 'zZ7Xvxc1Ta8',
         'fr': 'I3zYgwRzvVM',
         'pt-br': 'lR8Ql_bxoZ4',
     },
