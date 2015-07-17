5c11d2eb6b278c3cf4a3fed0692b5c017c7595dd
Giorgos Logiotatidis <seadog@sealabs.net>
2015-07-17 12:44:53 +0300

    Upgrade to Deis client 1.8.0

--- bin/jenkins/push2deis.sh
+++ bin/jenkins/push2deis.sh
@@ -12,7 +12,7 @@ set -ex
 TDIR=`mktemp -d`
 virtualenv $TDIR
 . $TDIR/bin/activate
-pip install deis==1.6.1
+pip install deis==1.8.0
 
 
 deis login $DEIS_CONTROLLER  --username $DEIS_USERNAME --password $DEIS_PASSWORD
